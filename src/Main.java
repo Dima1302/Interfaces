d75a8d53aca9e8cd34a8d4e34ca072fe3482343a
Dmitriy <Dimitriy303@yandex.ru>
2022-07-16 01:04:25 +0900

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Car car = new Car("KIA", 4);
+        Car car2 = new Car("HYUNDAI", 4);
+
+
+        Truck truck = new Truck("FORD", 6);
+        Truck truck2 = new Truck("CHEVROLET", 8);
+
+
+        Bicycle bicycle = new Bicycle("АИСТ", 2);
+        Bicycle bicycle2 = new Bicycle("КАМА", 2);
+
+
+        ServiceStation station = new ServiceStation();
+        station.check(car, truck, bicycle);
+        station.check(car2, truck2, bicycle2);
+
+
     }
 }
+
+
